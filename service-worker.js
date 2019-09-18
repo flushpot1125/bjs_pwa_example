27e508705255185086a984bb50b11b224175b183
Limes2018 <crescentblue1122@gmail.com>
2019-09-18 19:02:59 +0900

    add cache

--- service-worker.js
+++ service-worker.js
@@ -1,10 +1,28 @@
 //Ref:https://qiita.com/umamichi/items/0e2b4b1c578e7335ba20
+
+//Ref:https://qiita.com/masanarih0ri/items/0845f312cff5c8d0ec60
+const STATIC_DATA = [
+  'index.html',
+  'model/museum.glb',
+  'js/TeleportCheck.js'
+];
 self.addEventListener('install', function(e) {
     console.log('[ServiceWorker] Install');
+    e.waitUntil(
+      caches.open('cache_v1').then(function(cache) {
+        return cache.addAll(STATIC_DATA);
+      })
+    );
   });
   
   self.addEventListener('activate', function(e) {
     console.log('[ServiceWorker] Activate');
   });
   
-  self.addEventListener('fetch', function(event) {});
+  self.addEventListener('fetch', function(event) {
+    event.respondWith(
+      caches.match(event.request).then(function(response) {
+        return response || fetch(event.request);
+      })
+    );
+  });
