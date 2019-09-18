1cb926805f4e760717543fd1c95e4cfe588c4773
Limes2018 <crescentblue1122@gmail.com>
2019-09-18 16:19:22 +0900

    Added serviceworker

---
+++ js/service-worker.js
@@ -0,0 +1,10 @@
+//Ref:https://qiita.com/umamichi/items/0e2b4b1c578e7335ba20
+self.addEventListener('install', function(e) {
+    console.log('[ServiceWorker] Install');
+  });
+  
+  self.addEventListener('activate', function(e) {
+    console.log('[ServiceWorker] Activate');
+  });
+  
+  self.addEventListener('fetch', function(event) {});
