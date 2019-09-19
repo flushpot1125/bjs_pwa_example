fbc1555d9846d49f677c63fbfa05d39270a975a5
Limes2018 <crescentblue1122@gmail.com>
2019-09-20 00:13:43 +0900

    mod for cache

--- service-worker.js
+++ service-worker.js
@@ -2,36 +2,45 @@
 
 //Ref:https://qiita.com/masanarih0ri/items/0845f312cff5c8d0ec60
 const STATIC_DATA = [
-  'index.html',
-  'model/museum.glb',
-  'babylonjs_js/babylon.js',
-  'babylonjs_js/babylonjs.loaders.min.js',
-  'babylonjs_js/babylonjs.material.min.js',
-  'babylonjs_js/babylon.gui.min.js',
-  'babylonjs_js/babylon.inspector.bundle.js',
-  'babylonjs_js/pep.js',
-  'js/TeleportCheck.js'
+  '/',
+  '/index.html',
+  '/model/museum.glb',
+  '/babylonjs_js/babylon.js',
+  '/babylonjs_js/babylonjs.loaders.min.js',
+  '/babylonjs_js/babylonjs.material.min.js',
+  '/babylonjs_js/babylon.gui.min.js',
+  '/babylonjs_js/babylon.inspector.bundle.js',
+  '/babylonjs_js/pep.js',
+  '/js/TeleportCheck.js'
 ];
-self.addEventListener('install', function(e) {
+self.addEventListener('install', e => {
     console.log('[ServiceWorker] Install');
 
     e.waitUntil(
-      caches.open('cache_v1').then(function(cache) {
-        return cache.addAll(STATIC_DATA);
+      caches.open('cache_v1').then(cache => {
+        return cache.addAll(STATIC_DATA)
+        .then(()=> self.skipWaiting());
       })
     );
 
   });
   
-  self.addEventListener('activate', function(e) {
+  self.addEventListener('activate', e => {
     console.log('[ServiceWorker] Activate');
+    e.waitUntil(self.clients.claim());
+    
   });
   
-  self.addEventListener('fetch', function(event) {
-    console.log(event.request.url);
-    event.respondWith(
-      caches.match(event.request).then(function(response) {
-        return response || fetch(event.request);
-      })
+  self.addEventListener('fetch', e=> {
+    console.log(e.request.url);
+    e.respondWith(
+      caches.open(cacheName)
+      .then(cache => cache.match(e.request, {ignoreSearch: true}))
+      .then(response => {
+      return response || fetch(e.request);
+    })
+     // caches.match(event.request).then(function(response) {
+     //   return response || fetch(event.request);
+     // })
     );
   });
