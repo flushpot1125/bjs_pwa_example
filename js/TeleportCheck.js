44f0d3ca4be237d2512ea6269f6bd2a065da639f
flushpot1125 <crescentblue1122@gmail.com>
2021-11-13 14:55:03 +0900

    Replaced from WebXR PG example

--- js/TeleportCheck.js
+++ js/TeleportCheck.js
@@ -1,38 +1,65 @@
 var canvas = document.getElementById("renderCanvas");
 
+var engine = null;
+var scene = null;
+var sceneToRender = null;
+var createDefaultEngine = function() { return new BABYLON.Engine(canvas, true, { preserveDrawingBuffer: true, stencil: true,  disableWebGL2Support: false}); };
 var createScene = async function () {
+
+    // This creates a basic Babylon Scene object (non-mesh)
     var scene = new BABYLON.Scene(engine);
-  //  scene.debugLayer.show();
 
-    var camera = new BABYLON.ArcRotateCamera('MainCamera1', 0, 0, 3, BABYLON.Vector3(0, 1.2, 0), scene, true);
-    camera.position = new BABYLON.Vector3(0, 1.2, -1.1); 
+    // This creates and positions a free camera (non-mesh)
+    var camera = new BABYLON.FreeCamera("camera1", new BABYLON.Vector3(0, 5, -10), scene);
 
+    // This targets the camera to scene origin
+    camera.setTarget(BABYLON.Vector3.Zero());
+
+    // This attaches the camera to the canvas
     camera.attachControl(canvas, true);
-    camera.inputs.attached.mousewheel.detachControl(canvas);
-    
-    var light1 = new BABYLON.HemisphericLight("light1", new BABYLON.Vector3(1, 1, 0), scene);
 
+    // This creates a light, aiming 0,1,0 - to the sky (non-mesh)
+    var light = new BABYLON.HemisphericLight("light", new BABYLON.Vector3(0, 1, 0), scene);
 
-   BABYLON.SceneLoader.ImportMeshAsync("", "./model/", "museum.glb", scene).then(function(result) {
+    // Default intensity is 1. Let's dim the light a small amount
+    light.intensity = 0.7;
 
-   });
+    // Our built-in 'sphere' shape.
+    var sphere = BABYLON.MeshBuilder.CreateSphere("sphere", {diameter: 2, segments: 32}, scene);
 
-   const xrHelper = await scene.createDefaultXRExperienceAsync({
-   // floorMeshes: [environment.ground]
-    });
+    // Move the sphere upward 1/2 its height
+    sphere.position.y = 1;
 
+    scene.createDefaultEnvironment();
 
+    // XR
+    const xrHelper = await scene.createDefaultXRExperienceAsync();
 
     return scene;
+
 };
+        window.initFunction = async function() {
+            
+            
+            var asyncEngineCreation = async function() {
+                try {
+                return createDefaultEngine();
+                } catch(e) {
+                console.log("the available createEngine function failed. Creating the default engine instead");
+                return createDefaultEngine();
+                }
+            }
 
-var engine = new BABYLON.Engine(canvas, true, { preserveDrawingBuffer: true, stencil: true });
-var scene = createScene();
-
-engine.runRenderLoop(function () {
-    if (scene) {
-        scene.render();
-    }
+            window.engine = await asyncEngineCreation();
+if (!engine) throw 'engine should not be null.';
+window.scene = createScene();};
+initFunction().then(() => {scene.then(returnedScene => { sceneToRender = returnedScene; });
+        
+    engine.runRenderLoop(function () {
+        if (sceneToRender && sceneToRender.activeCamera) {
+            sceneToRender.render();
+        }
+    });
 });
 
 // Resize
